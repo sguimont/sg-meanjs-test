89615d756d71846e4af115d23c0116286203d9f1
Sebastien Guimont <sebastieng@sympatico.ca>
2014-05-04 19:14:16 -0400

    fix some issues related to directives

--- config/init.js
+++ config/init.js
@@ -21,12 +21,12 @@ module.exports = function() {
     		if(process.env.NODE_ENV) {
     			console.log('\x1b[31m', 'No configuration file found for "' + process.env.NODE_ENV + '" envrionment using develpoment instead');
     		} else {
-    			console.log('\x1b[31m', 'NODE_ENV is not defined! Using default develpoment envrionment');
+    			console.log('\x1b[31m', 'NODE_ENV is not defined! Using default develpoment environment');
     		}
 
     		process.env.NODE_ENV = 'development';
     	} else {
-    		console.log('\x1b[7m', 'Application loaded using the "' + process.env.NODE_ENV + '" envrionment configuration');
+    		console.log('\x1b[7m', 'Application loaded using the "' + process.env.NODE_ENV + '" environment configuration');
     	}
     	console.log('\x1b[0m');
     });
